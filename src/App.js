4450071b4af2937d3237ce84bdc0c80f73b65b1a
edk21 <edwinmcken@gmail.com>
2023-04-07 22:16:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ import Location from './components/Location/Location';
 
 function App() {
   const apiUrl = "https://test-api-ckcd.onrender.com"
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await axios.get(
-  //       "site--flink-back-end--jd2xpg8l2jfs.code.run"
-  //     );
-  //     console.log("response: ", response);
-  //   };
-  //   fetchData();
-  // });
+
   return (
     <Router>
       <Routes>
